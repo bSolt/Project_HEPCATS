29e5c9f63947174445a6d6f706736e2e652ba04b
Braden Solt <brso4254@colorado.edu>
2019-02-14 13:01:43 -0700

    updated detection.py

--- ips/detection.py
+++ ips/detection.py
@@ -2,7 +2,7 @@
 
 import tensorflow as tf
 import numpy as np
-import os, rawpy
+import os, rawpy, zlib
 from PIL import Image
 #from ips_helper import *
 
@@ -19,22 +19,35 @@ run = True
 # Open the input pipe for reading and writing
 # pipe = open(INPUT_PIPE,'r+b')
 
+def auto_crop(image):
+	return croppped_image, error_code
+
 while(run):
-	# raw = pipe.read()
+	# raw = read_from_pipe(INPUT_PIPE)
+
+	# PIPES
+	# Find out if this works and is blocking
 	raw = rawpy.imread(INPUT_PIPE)
-	# I hope that the above is blocking, but not sure exactly
-	rgb = raw.postprocess()
-	# imar = auto_crop(rgb)
+	rgb_full = raw.postprocess()
+	# KIAN'S FUNCTION
+	# concerned about run time, might need to fix later
+	# in the future we might 
+	rgb_crop, error_code = auto_crop(rgb_full)
+
 	rgb_small = np.array(
-		Image.fromarray(rgb).resize((256,256))
+		Image.fromarray(rgb_crop).resize((256,256))
 		)
 	rgb_small = np.expand_dims(rgb_small,0)
 	pred = model.predict(rgb_small)
+	# MATT COMPRESSION
 	# input has to be read-only, tests use
-	# compr_stream = compress_all(rgb,zlib.Z_BEST_COMPRESSION)
+	# might need to change the input to this
+	# might have to save to file, then read as read only type
+	compr_stream = zlib.compress(rgb_crop,zlib.Z_BEST_COMPRESSION)
 
 	print(f'[INFO] {INPUT_PIPE} has rated as {pred}')
 
+	## Write to pipe part
 	with open(OUT_TEST,'wb') as pipe:
 		if (pred >= THRESHOLD):
 			np.save(pipe,rgb)
