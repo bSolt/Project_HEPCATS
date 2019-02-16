90dd0fcb5f87b28a03132d7f293f00f8701f5c34
Christopher Peercy <chpe1358@colorado.edu>
2019-02-15 18:41:38 -0700

    Major changes to GetImages. Added large amounts of error handling, possibly fixed image conversion problem into RBG8 pixelformat. Removed RunSingleCamera, added additional support functions for device info printing, moved all initializtion to occur before acquisition loop.

--- imaging/fsw/getImages/GetImages_C.cpp
+++ imaging/fsw/getImages/GetImages_C.cpp
@@ -6,19 +6,19 @@ Project HEPCATS
 
 GetImages.cpp
 This function commands the camera to get images at a particular rate. 
-This file heavily references and closely the Spinnaker SDK examples provided by 
+This file heavily references and closely follows the Spinnaker SDK examples provided by 
 Point Grey Research.
 
-INPUTS: Imaging rate in Hz
+INPUTS: Imaging rate in Hz //defunct
 Returns: int, 0 success, 1 error
 
 */
 
-#include "SpinnakerC.h"
+#include "SpinnakerC.h" //Main SDK headers
 #include "stdio.h"
 #include "string.h"
 #include "stdlib.h" //Needed for memory allocation
-#include "fcntl.h" //Needed for writing buffer to files
+//#include "fcntl.h" //Needed for writing buffer to files
 #include "unistd.h" //Needed to facilitate waiting between pictures.
 #include "time.h" //Needed for timestamping files.
 
@@ -30,42 +30,182 @@ Returns: int, 0 success, 1 error
 //Extend the buffer length
 #define MAX_BUFF_LEN 256
 
+
+
+
+
+
+
+//Support function for printing device info
+bool8_t IsAvailableAndWritable(spinNodeHandle hNode, char nodeName[])
+{
+    bool8_t pbAvailable = False;
+    spinError err = SPINNAKER_ERR_SUCCESS;
+    err = spinNodeIsAvailable(hNode, &pbAvailable);
+    if (err != SPINNAKER_ERR_SUCCESS)
+    {
+        printf("Unable to retrieve node availability (%s node), with error %d...\n\n", nodeName, err);
+    }
+
+    bool8_t pbWritable = False;
+    err = spinNodeIsWritable(hNode, &pbWritable);
+    if (err != SPINNAKER_ERR_SUCCESS)
+    {
+        printf("Unable to retrieve node writability (%s node), with error %d...\n\n", nodeName, err);
+    }
+    return pbWritable && pbAvailable;
+}
+
+
+
+
+
+
+
+//Support function for printing device info
+bool8_t IsAvailableAndReadable(spinNodeHandle hNode, char nodeName[])
+{
+    bool8_t pbAvailable = False;
+    spinError err = SPINNAKER_ERR_SUCCESS;
+    err = spinNodeIsAvailable(hNode, &pbAvailable);
+    if (err != SPINNAKER_ERR_SUCCESS)
+    {
+        printf("Unable to retrieve node availability (%s node), with error %d...\n\n", nodeName, err);
+    }
+
+    bool8_t pbReadable = False;
+    err = spinNodeIsReadable(hNode, &pbReadable);
+    if (err != SPINNAKER_ERR_SUCCESS)
+    {
+        printf("Unable to retrieve node readability (%s node), with error %d...\n\n", nodeName, err);
+    }
+    return pbReadable && pbAvailable;
+}
+
+
+
+
+
+
+
+
+// This function handles the error prints when a node or entry is unavailable or
+// not readable/writable on the connected camera
+void PrintRetrieveNodeFailure(char node[], char name[])
+{
+    printf("Unable to get %s (%s %s retrieval failed).\n\n", node, name, node);
+}
+
+
+
+
+
+
+
+
+
+//Print device information
+spinError PrintDeviceInfo(spinNodeMapHandle hNodeMap)
+{
+    spinError err = SPINNAKER_ERR_SUCCESS;
+    unsigned int i = 0;
+
+    printf("\n*** DEVICE INFORMATION ***\n\n");
+
+    // Retrieve device information category node
+    spinNodeHandle hDeviceInformation = NULL;
+
+    err = spinNodeMapGetNode(hNodeMap, "DeviceInformation", &hDeviceInformation);
+    if (err != SPINNAKER_ERR_SUCCESS)
+    {
+        printf("Unable to retrieve node. Non-fatal error %d...\n\n", err);
+        return err;
+    }
+
+    // Retrieve number of nodes within device information node
+    size_t numFeatures = 0;
+
+    if (IsAvailableAndReadable(hDeviceInformation, "DeviceInformation"))
+    {
+        err = spinCategoryGetNumFeatures(hDeviceInformation, &numFeatures);
+        if (err != SPINNAKER_ERR_SUCCESS)
+        {
+            printf("Unable to retrieve number of nodes. Non-fatal error %d...\n\n", err);
+            return err;
+        }
+    }
+    else
+    {
+        PrintRetrieveNodeFailure("node", "DeviceInformation");
+        return SPINNAKER_ERR_ACCESS_DENIED;
+    }
+
+    // Iterate through nodes and print information
+    for (i = 0; i < numFeatures; i++)
+    {
+        spinNodeHandle hFeatureNode = NULL;
+
+        err = spinCategoryGetFeatureByIndex(hDeviceInformation, i, &hFeatureNode);
+        if (err != SPINNAKER_ERR_SUCCESS)
+        {
+            printf("Unable to retrieve node. Non-fatal error %d...\n\n", err);
+            continue;
+        }
+spinNodeType featureType = UnknownNode;
+
+        //get feature node name
+        char featureName[MAX_BUFF_LEN];
+        size_t lenFeatureName = MAX_BUFF_LEN;
+        err = spinNodeGetName(hFeatureNode, featureName, &lenFeatureName);
+        if (err != SPINNAKER_ERR_SUCCESS)
+        {
+            strcpy(featureName, "Unknown name");
+        }
+
+        if (IsAvailableAndReadable(hFeatureNode, featureName))
+        {
+            err = spinNodeGetType(hFeatureNode, &featureType);
+            if (err != SPINNAKER_ERR_SUCCESS)
+            {
+                printf("Unable to retrieve node type. Non-fatal error %d...\n\n", err);
+                continue;
+            }
+        }
+        else
+        {
+            printf("%s: Node not readable\n", featureName);
+            continue;
+        }
+
+        char featureValue[MAX_BUFF_LEN];
+        size_t lenFeatureValue = MAX_BUFF_LEN;
+
+        err = spinNodeToString(hFeatureNode, featureValue, &lenFeatureValue);
+        if (err != SPINNAKER_ERR_SUCCESS)
+        {
+            strcpy(featureValue, "Unknown value");
+        }
+
+        printf("%s: %s\n", featureName, featureValue);
+    }
+    printf("\n");
+
+    return err;
+}
+
+
+
+
+
+
+
+
+
 //Main Aquisistion function
 spinError Acquire(spinCamera hCam, spinNodeMapHandle hNodeMap, spinNodeMapHandle hNodeMapTLDevice)
 {
 
 	spinError err = SPINNAKER_ERR_SUCCESS;
-	//Commented out for WIP to minimize camera state commanding inside aquisition function
-	/*printf("\n STARTING IMAGE AQUISISTION \n");
-
-	//Set the camera to single aquisition mode
-
-	//Make node handles
-	spinNodeHandle hAcquisitionMode = NULL;
-	spinNodeHandle hAcquisitionModeSingle = NULL;
-	int64_t acquisitionModeSingle = 0;
-	
-	err = spinNodeMapGetNode(hNodeMap, "AcquisitionMode", &hAcquisitionMode);
-	printf("Got acquisition mode node map\n");
-	err = spinEnumerationGetEntryByName(hAcquisitionMode, "SingleFrame", &hAcquisitionModeSingle);
-	if (err != SPINNAKER_ERR_SUCCESS)
-	{
-		printf("Error setting camera to single frame aquisition\n");
-		return err;
-	}
-	else
-	{
-		printf("Single frame aquisition set successfully\n");
-	}
-	
-	err = spinEnumerationEntryGetIntValue(hAcquisitionModeSingle, &acquisitionModeSingle);
-	//Set the integer as the new enumeration value:
-	err = spinEnumerationSetIntValue(hAcquisitionMode, acquisitionModeSingle);
-	if (err != SPINNAKER_ERR_SUCCESS)
-	{
-		printf("Could not set camera to single frame");
-		return err;
-	}*/
 	
 	//Take a picture
 	err = spinCameraBeginAcquisition(hCam);
@@ -73,6 +213,10 @@ spinError Acquire(spinCamera hCam, spinNodeMapHandle hNodeMap, spinNodeMapHandle
 	{
 		printf("Couldn't start acquisition. Sorry bout that...");
 		return err;
+	}
+	else
+	{
+		printf("Successfully commanded camera acquisition start");
 	}
 	
 	spinImage hResultImage=NULL;
@@ -89,14 +233,28 @@ spinError Acquire(spinCamera hCam, spinNodeMapHandle hNodeMap, spinNodeMapHandle
 	spinImage hConvertedImage = NULL;
 	
 	err = spinImageCreateEmpty(&hConvertedImage);
-	err = spinImageConvert(hResultImage, PixelFormat_BayerRG8, hConvertedImage);
-
-
+	if (err != SPINNAKER_ERR_SUCCESS)
+	{
+		printf("FATAL: Couldn't create an empty image to hold converted image \n");
+		return err;
+	}
+	
+	err = spinImageConvert(hResultImage, PixelFormat_RGB8, hConvertedImage);
+	if (err != SPINNAKER_ERR_SUCCESS)
+	{
+		printf("FATAL: Image conversion failed\n");
+		//return err;
+	}
 
 	//For now, save to a file
 	char filename[MAX_BUFF_LEN];
-	sprintf(filename,"newImage");
-	err = spinImageSave(hConvertedImage, filename, RAW);
+	time_t CurrentTime=time(NULL);
+	char* strTime;
+	strTime=ctime(&CurrentTime);
+	
+	sprintf(filename,"Image" );
+	err = spinImageSave(hConvertedImage, filename, JPEG);
+	sleep(1);
 		if(err != SPINNAKER_ERR_SUCCESS)
 		{
 		printf("Failed to save the Image\n");
@@ -109,97 +267,46 @@ spinError Acquire(spinCamera hCam, spinNodeMapHandle hNodeMap, spinNodeMapHandle
 	//Clean up, clean up, everybody do your share
 	printf("Cleaning up image and camera handles\n");
 	err = spinImageDestroy(hConvertedImage);
+	err = spinImageDestroy(&hResultImage);
+        if (err != SPINNAKER_ERR_SUCCESS)
+        {
+                printf("NON-FATAL: Could not destroy result image\n");
+	}
 	err = spinCameraEndAcquisition(hCam);
 	if (err != SPINNAKER_ERR_SUCCESS)
 	{
-		printf("Could not end image aquisition! Future images may not capture");
+		printf("Could not end image aquisition! Future images may not capture\n");
 	}
 	else
 	printf("Camera handles cleaned up\n");
 
-	
-
-
-	//Write raw to buffer
-        /*
-		//Open the file read only
-		FILE *imgFile=fopen("newImage.raw", O_RDONLY);
-		if (imgFile < 0)
-			printf("Could not get file descriptor for raw image");
-		else
-		{
-		printf("Got file descriptor for image file");
-		size_t imageSize=2304000;
-		//Open the buffer file for writing
-		const char *Buff_location="/dev/rtp1";
-		int destFile = open(Buff_location, O_CREAT, O_WRONLY);
-		int ferr;
-		ferr = write(destFile, imgFile, imageSize); //All raw images are exactly 2304000 bytes so long as bRG8 is set as pixelspace
-		if (ferr > 0)
-			printf("\nSuccessfuly wrote %d to buffer\n", ferr);
-		else
-			printf("\n Could not write to buffer\n");
-		ferr=fclose(imgFile);
-		ferr=close(destFile);
-		}
-
-*/
-
-//Cleanup the result image
-	err = spinImageDestroy(&hResultImage);
-
 	return err;
 } //END OF FUNCTION ACQUIRE
 
-/*spinError RunSingleCamera(spinCamera hCam)
-{
-	spinError err= SPINNAKER_ERR_SUCCESS;
-	
-	//Need the transport layer nodemap for later:
-	spinNodeMapHandle hNodeMapTLDevice=NULL;
-	err = spinCameraGetTLDeviceNodeMap(hCam, &hNodeMapTLDevice);
-	
-	//Initialize camera
-	err= spinCameraInit(hCam);
-	if(err!= SPINNAKER_ERR_SUCCESS)
-	{
-		printf("Could not initialize camera. Aborting cause Chris' code is probs broken \n");
-		printf("Or someone just doesn't know how to use a USB port\n");
-		printf("Make sure the camera is NOT plugged into the paddleboard\n");
-		printf("The camera has to be plugged into the 3.1 port on the IEU\n");
-		printf("The first is more likely tho, cause I have like, ZERO idea what I'm doing\n");
-		return err;
-	}
-	
-	//Get the full nodeMap using GenICam
-	spinNodeMapHandle hNodeMap = NULL;
-	
-	err = spinCameraGetNodeMap(hCam, &hNodeMap);
-	
-	//Call that good ol acquisistion function now and get a picture.
-	err = Acquire(hCam, hNodeMap, hNodeMapTLDevice);
-	/*if(err!=SPINNAKER_ERR_SUCCESS)
-	{
-		printf("Aquisition returned an error. Try agian or turning it off then on again\n");
-		return err;
-	}
-	//Now that we have a picture, we gotta release the camera
-	err = spinCameraDeInit(hCam);
-	
-	return err;
-} //END OF FUNCTION RunSingleCamera
-*/
-int main(/*int argc, char** argv*/)
+
+
+
+
+
+
+
+
+
+int main(int argc, char** argv)
 {
 	spinError err=SPINNAKER_ERR_SUCCESS;
 	//check permissions
 	{
 	FILE *temp;
-	temp=fopen("test.txt","w+")
+	temp=fopen("test.txt","w+");
 	if (temp == NULL)
 	{
 		printf("Cannot save images in current folder. Invalid permissions\n");
 		return SPINNAKER_ERR_ACCESS_DENIED;
+	}
+	else
+	{
+		printf("Permissions check complete. Write permissions OK");
 	}
 	fclose(temp);
 	remove("test.txt");
@@ -224,10 +331,7 @@ int main(/*int argc, char** argv*/)
 		printf("No cameras attached.Hook up a camera dummy \n");
 		return -1;
 	}
-	// Get some images every so often.
 	//We'll initialize the camera here
-	int imageCount = 1; //Number of pictures we want to take
-	int imageInterval = 5; //Number of seconds to wait between images.
 	
 	//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@//
 	//FROM RUN SINGLE CAMERA//
@@ -237,9 +341,13 @@ int main(/*int argc, char** argv*/)
 	//Need TL nodemaps
 	spinNodeMapHandle hNodeMapTLDevice=NULL;
         err = spinCameraGetTLDeviceNodeMap(hCamera, &hNodeMapTLDevice);
+	
+	//Print device information to get initial state
+	printf("Getting initial camera state:\n");
+	err = PrintDeviceInfo(hNodeMapTLDevice);
 
         //Initialize camera
-        err= spinCameraInit(hCam);
+        err= spinCameraInit(hCamera);
         if(err!= SPINNAKER_ERR_SUCCESS)
         {
 		//If we can't initiallize, yell at the user a bit
@@ -255,13 +363,12 @@ int main(/*int argc, char** argv*/)
         spinNodeMapHandle hNodeMap = NULL;
 
         err = spinCameraGetNodeMap(hCamera, &hNodeMap);
-
+	
 	//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@//
 	//END FROM RUN SINGLE CAMERA//
 
 	//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@//
 	//FROM MAIN ACQUIRE FUNCTION//
-	printf("\n STARTING IMAGE AQUISISTION \n");
 
         //Set the camera to single aquisition mode
 
@@ -269,7 +376,7 @@ int main(/*int argc, char** argv*/)
         spinNodeHandle hAcquisitionMode = NULL;
         spinNodeHandle hAcquisitionModeSingle = NULL;
         int64_t acquisitionModeSingle = 0;
-
+	
         err = spinNodeMapGetNode(hNodeMap, "AcquisitionMode", &hAcquisitionMode);
         printf("Got acquisition mode node map\n");
         err = spinEnumerationGetEntryByName(hAcquisitionMode, "SingleFrame", &hAcquisitionModeSingle);
@@ -283,6 +390,7 @@ int main(/*int argc, char** argv*/)
                 printf("Single frame aquisition set successfully\n");
         }
 
+	
         err = spinEnumerationEntryGetIntValue(hAcquisitionModeSingle, &acquisitionModeSingle);
         //Set the integer as the new enumeration value:
         err = spinEnumerationSetIntValue(hAcquisitionMode, acquisitionModeSingle);
@@ -294,16 +402,26 @@ int main(/*int argc, char** argv*/)
 	//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@//
 	//END FROM MAIN AQUIRE FUNCTION//
 
-	//Ready to aquire. 
+	//Done with init stuff, get camera info again
+	printf("Finished setting camera to SFA state\n");
 
+	
+	//Ready to aquire. 
+	int imageCount = 10; //Number of pictures we want to take
+        int imageInterval = 1; //Number of seconds to wait between images.
 	for(int i=0; i < imageCount; i++)
 	{
 		printf("Taking picture %d \n", i);
 		err = Acquire(hCamera, hNodeMap, hNodeMapTLDevice);
-		//Release the camera
-		printf("Waiting for %d seconds", imageInterval);
+		if (err != SPINNAKER_ERR_SUCCESS)
+		{
+			printf("Fatal image aquisition error, dumping device state:\n");
+			err = PrintDeviceInfo(hNodeMapTLDevice);
+			return -1;
+		}
+		printf("Waiting for %d seconds\n", imageInterval);
 		//wait for a the interval
-		//sleep(imageInterval);
+		sleep(imageInterval);
 	}
 	//Clean up the system and release camera referances
 	
@@ -314,7 +432,7 @@ int main(/*int argc, char** argv*/)
 	
 	//Release the system
 	err = spinSystemReleaseInstance(hSystem);
-	printf("\n Extraordinary. Everything is done.");
+	printf("\n Extraordinary. Everything is done.\n");
 	return err;
 }
 
