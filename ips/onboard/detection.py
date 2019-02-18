b3e2b675e0586763f04ebfb2b9277e26402f5151
Braden Solt <brso4254@colorado.edu>
2019-02-17 18:58:41 -0700

    Trying to integrate ips

--- ips/onboard/detection.py
+++ ips/onboard/detection.py
@@ -5,11 +5,12 @@ import numpy as np
 import os, rawpy, zlib
 from PIL import Image
 from FixedBufferReader import FixedBufferReader
+from croppingScript import auto_crop
 #from ips_helper import *
 
-MODEL_FILE = "models/winter_model_1.h5"
+MODEL_FILE = "../models/winter_model_1.h5"
 # INPUT_PIPE = "/dev/rtp0"
-INPUT_PIPE = "/dev/pts/5"
+INPUT_PIPE = "/dev/pts/4"
 THRESHOLD = 0.5
 OUT_TEST = "output.npy"
 BYTES = 9861950; #This is for the testing image
@@ -41,7 +42,7 @@ while(run):
 	# KIAN'S FUNCTION
 	# concerned about run time, might need to fix later
 	# in the future we might 
-	# rgb_crop, error_code = auto_crop(rgb_full)
+	rgb_crop = auto_crop(rgb_full)
 
 	# for now
 	rgb_crop = rgb_full
