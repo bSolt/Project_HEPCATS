12744386e1e601e2eb941a4d6947b95993a46dd3
Kian Tanner <kian0tanner@gmail.com>
2019-02-17 21:37:06 -0700

    Changed to be a function
    Cropping works, pointing check works

--- ips/onboard/croppingScript.py
+++ ips/onboard/croppingScript.py
@@ -4,10 +4,7 @@ import numpy as np
 
 
 def auto_crop(im_array):
-    # Meta
-    fileName = 'image6.png'
-
-    # Predicted Radii
+    # Radii Parameters
     startingRadius = 800
     radiusDelta = 25
     endingRadius = 1500
@@ -25,27 +22,28 @@ def auto_crop(im_array):
     borderPad = 300
 
     # fix image
-    if (im_array.shape[2] > 1):
-        I_bw = cv2.cvtColor(im_array,cv2.COLOR_RGB2GRAY)
-    else:
-        I_bw = im_array
+    I_bw = cv2.cvtColor(im_array, cv2.COLOR_RGB2GRAY)
     I_bw = cv2.GaussianBlur(I_bw, (blurSigma, blurSigma), 0)
     I_bw = cv2.GaussianBlur(I_bw, (blurSigma, blurSigma), 0)
     I_bw = cv2.GaussianBlur(I_bw, (blurSigma, blurSigma), 0)
 
-    I_col = cv2.imread(fileName)
+    I_col = im_array
     I_col = cv2.copyMakeBorder(I_col, borderPad, borderPad, borderPad, borderPad, cv2.BORDER_CONSTANT, value=BLACK)
 
 
     # Find Circles
+    errorCode = 0
+    pointingCode = 0
     while True:
         if accumulatorThresh < minAccumulatorVotes:
             accumulatorThresh = startingAccumulatorVotes
             startingRadius = startingRadius+radiusDelta
             
         if startingRadius > endingRadius:
-            print("No circle under specified radius found")
-            raise SystemExit(0)
+            print("No circle under specified radius found.")
+            I_crop = 0
+            errorCode = 1
+            break
         
         circles = cv2.HoughCircles(I_bw, cv2.HOUGH_GRADIENT, 2, 5, param1=sens, param2=accumulatorThresh, minRadius=startingRadius-radiusDelta, maxRadius=startingRadius+radiusDelta)
         try:
@@ -63,28 +61,39 @@ def auto_crop(im_array):
         else:
             break
 
+    if errorCode == 0:
+        circles = np.uint16(np.around(circles))
+        meanC = np.mean(circles, 1)
+        meanX = int(np.round(meanC[0,0], 0))
+        meanY = int(np.round(meanC[0,1], 0))
+        meanR = int(np.round(meanC[0,2], 0))
+        deltaCrop = int((deltaCropRatio+1)*meanR)
+        height, width, depth = I_col.shape
+        I_crop = I_col[meanY-deltaCrop+borderPad:meanY+deltaCrop+borderPad, meanX-deltaCrop+borderPad:meanX+deltaCrop+borderPad]
 
-    circles = np.uint16(np.around(circles))
-    meanC = np.mean(circles, 1)
-    meanX = int(np.round(meanC[0,0], 0))
-    meanY = int(np.round(meanC[0,1], 0))
-    meanR = int(np.round(meanC[0,2], 0))
-    deltaCrop = int((deltaCropRatio+1)*meanR)
-    height, width, depth = I_col.shape
-    I_crop = I_col[meanY-deltaCrop:meanY+deltaCrop, meanX-deltaCrop:meanX+deltaCrop]
-    cv2.imwrite("cropped.png", I_crop)
+        # Pointing Check
+        if (meanX-meanR) < borderPad: # Need to point better, Earth is too far left
+            pointingCode = 1
+        elif (meanX+meanR) > (width-2*borderPad): # Need to point better, Earth is too far right
+            pointingCode = 2
+        elif (meanY-meanR) < borderPad: # Need to point better, Earth is too far up
+            pointingCode = 3
+        elif (meanY+meanR) > (height-2*borderPad): # Need to point better, Earth is too far down
+            pointingCode = 4
 
-        
-    # Pointing Check
-    if (meanX-meanR) < borderPad:
-        print("Point better. Earth is too far left.")
-    elif (meanX+meanR) > (width-borderPad):
-        print("Point better. Earth is too far right.")
-    elif (meanY-meanR) < borderPad:
-        print("Point better. Earth is too far up.")
-    elif (meanY+meanR) > (height-borderPad):
-        print("Point better. Earth is too far down.")
-    else:
-        print("Good pointing.")
+    return [I_crop, pointingCode, errorCode]
+    
+    
+# Script
+fileName = 'image9.png'
+in_image = cv2.imread(fileName)
+[out_image, pointingError, errorCode] = auto_crop(in_image)
 
-    return I_crop
+if errorCode == 1:
+    print("Error: No circles found.")
+else:
+    print("Image cropped successflly.")
+    cv2.imwrite("cropped.png", out_image)
+    if pointingError != 0:
+        print("Camera not pointing correctly, error code %s." % pointingError)
+
