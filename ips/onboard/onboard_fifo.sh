81f686df5350e57f36a704c70d71387598e7f6c4
IEU <brso4254@colorado.edu>
2019-02-28 16:07:48 -0700

    Added comments

--- ips/onboard/onboard_fifo.sh
+++ ips/onboard/onboard_fifo.sh
@@ -3,6 +3,7 @@
 IMG="../RAW/IMG_6591.CR2"
 PIPE="./pipe.fifo"
 
+# Make fifo pipe if necessary
 if [ ! -f $PIPE ]; then
 	echo "Making $PIPE"
 	mkfifo $PIPE
@@ -13,6 +14,8 @@ echo "Running C Script"
 ./write_pipe $IMG $PIPE &
 
 echo "Running Python Script"
+# options to python script are [pipe name] [image format] [debug]
+# call ips script with the test pipe, test format, and debug flag
 python3 ips_ieu_script.py $PIPE test 1
 
 
