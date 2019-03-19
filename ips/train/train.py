c4b914aeac8aa3cb468fa72efa2e94c2b540647a
Braden Solt <brso4254@colorado.edu>
2019-03-19 15:51:08 -0600

    plot title fix

--- ips/train/train.py
+++ ips/train/train.py
@@ -396,7 +396,7 @@ def main():
       )
     pres_plot.plot_history(h2.history,
       color='white',
-      title='Training on Augmented Data with augmentation',
+      title=f'Training on Augmented Data with Augmentation ({ft_option})',
       save=psname+'_fine.png')
   else:
     print('[MAIN] Training classifier on augmented data w/o fine-tuning')
