c692234e64fac5554c4ba882a8228a9f23588fad
besp9510 <36608012+besp9510@users.noreply.github.com>
2019-03-22 13:41:10 -0600

    Corrected expected msg size

--- cdh/ieu/fsw/src/tasks/read_apps/img/read_img.c
+++ cdh/ieu/fsw/src/tasks/read_apps/img/read_img.c
@@ -137,7 +137,7 @@ void read_img(void) {
     // Synchronize with IPS:
     // (Wait IPS to be ready to receive and process images. Wait for message
     // indicating that IPS is ready to receive images via real-time message)
-    ret_val = rt_pipe_read(&ips_msg_pipe,&ips_ret,sizeof(ips_ret),\
+    ret_val = rt_pipe_read(&ips_msg_pipe,&ips_ret,1,\
         TM_INFINITE);
 
     // Print:
