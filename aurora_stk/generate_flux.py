04439d46836aed5bb77d3f75051bb87d8e80e9e9
Braden Solt <brso4254@colorado.edu>
2019-04-03 12:51:51 -0600

    Updates to Aurora image generating code

--- aurora_stk/generate_flux.py
+++ aurora_stk/generate_flux.py
@@ -1,3 +1,4 @@
+from __future__  import print_function
 from ovationpyme import ovation_prime, ovation_utilities
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
@@ -5,10 +6,22 @@ import datetime, aacgmv2
 import numpy as np
 from scipy.interpolate import griddata, SmoothSphereBivariateSpline
 
-def get_flux_geo(dt, atype="diff", jtype="electron energy flux"):
-	estimator = ovation_prime.FluxEstimator(atype,jtype)
-
-	mlatgrid,mltgrid,fluxgrid = estimator.get_flux_for_time(dt)
+def get_flux_geo(dt):
+	atypes=['diff','mono','wave']
+	jtype='electron energy flux'
+	fluxgrid = None
+	# Iterate over types
+	for atype in atypes:
+		print("Generating fluxes for {} type aurora".format(atype),end="\r")
+		# create estimator
+		estimator = ovation_prime.FluxEstimator(atype,jtype)
+		mlatgrid,mltgrid,fg = estimator.get_flux_for_time(dt)
+		# Create fluxgrid in the first iteration
+		if fluxgrid is None:
+			fluxgrid = np.zeros(fg.shape)
+		# Add to flux
+		fluxgrid += fg
+	print("Converting to GEO coordinates", end="\r")
 	# Convert mag local time to magnetic longitude
 	mlongrid = aacgmv2.convert_mlt(mltgrid,dt,m2a=True)
 	# Convert CGM to GEO
@@ -20,45 +33,50 @@ def get_flux_geo(dt, atype="diff", jtype="electron energy flux"):
 
 # Main program
 if __name__ == "__main__":
-	cutoff = 15
-	
-	dt = datetime.datetime(2003,05,04,20,0,0)
+	# make all bins below cutoff black
+	cutoff = 0
+	#NOTE: flux values are erg/cm^2/s
+	# bottom flux value
+	minv = 0.1
+	# max brightness flux value
+	max_bright_flux = 4
 
-	lat_grid, lon_grid, fluxgrid = get_flux_geo(dt)
-
-	# Convert to finer spherical grid
-	# Set up the spline interpolator
-	# deg = np.pi/180
-	# spline = SmoothSphereBivariateSpline(
-	# 	deg*lat_grid.flatten(), #theta
-	# 	deg*lon_grid.flatten()+np.pi, # phi
-	# 	fluxgrid.flatten(),s=3.5)
-	# scale_lat = np.linspace(40*deg,90*deg,256); scale_lon = np.linspace(-180*deg,180*deg,400)+np.pi;
-	# grid_flux = spline(scale_lat,scale_lon)
-	# Convert to known GEO grid
-	points = np.transpose([lon_grid.flatten(),lat_grid.flatten()])
-	grid_lat, grid_lon = np.meshgrid(np.linspace(40,90,256),np.linspace(-180,180,400))
-	grid_flux = griddata(points, fluxgrid.flatten(), (grid_lon, grid_lat), 
-		method='cubic', fill_value=0)
-	im_flux = np.transpose(grid_flux)
-	for minv in [0.15]:
-		# Create the brightness factor
-		max_bright_flux = 2
-		# flux_factor = np.transpose(grid_flux)/max_bright_flux #- subtract/256.
-		# bound the flux factor to [0,1]
-		# flux_factor[flux_factor>1] = 1
-		# flux_factor[flux_factor<0] = 0
-
-		#Create black image
-		# out_im = np.zeros(flux_factor.shape + (3,),dtype=np.uint8)
-		# define color to use
-		green = (np.array([189,255,0])*1./256).reshape((1,3))
-		colors=np.linspace(0,1,256).reshape((256,1))*green
-		colors[:cutoff,:] = [0,0,0]
-		cmap = ListedColormap(colors,
+	# define color to use
+	green = (np.array([189,255,0])*1./256).reshape((1,3))
+	colors=(np.linspace(0,1,256)**2).reshape((256,1))*green
+	cmap = ListedColormap(colors,
 			name='aurora')
 
-		sn = 'ovation_flux_{}_v{}.png'.format(dt.isoformat(),minv)
+	
+	for hour in np.arange(0,24,0.25): # Iterate every 15
+		dt = datetime.datetime(2003,10,29,0,0,0) + datetime.timedelta(hours=hour)
+		# Get the raw data
+		lat_grid, lon_grid, fluxgrid = get_flux_geo(dt)
+
+		# Convert to finer spherical grid
+		# Set up the spline interpolator
+		# deg = np.pi/180
+		# spline = SmoothSphereBivariateSpline(
+		# 	deg*lat_grid.flatten(), #theta
+		# 	deg*lon_grid.flatten()+np.pi, # phi
+		# 	fluxgrid.flatten(),s=3.5)
+		# scale_lat = np.linspace(40*deg,90*deg,256); scale_lon = np.linspace(-180*deg,180*deg,400)+np.pi;
+		# grid_flux = spline(scale_lat,scale_lon)
+
+		# Convert to known GEO grid
+		print("Interpolating to output grid", end='\r')
+		# create list of coordnation
+		points = np.transpose([lon_grid.flatten(),lat_grid.flatten()])
+		# create desired grid coordinates
+		grid_lat, grid_lon = np.meshgrid(np.linspace(40,90,256),np.linspace(-180,180,400))
+		# interpolate data onto new grid
+		grid_flux = griddata(points, fluxgrid.flatten(), (grid_lon, grid_lat), 
+			method='cubic', fill_value=0)
+		# transpose so the image is the right direction
+		im_flux = np.transpose(grid_flux)
+		# Generate save name
+		sn = 'ovation_flux_{}.png'.format(dt.isoformat()).replace(':','_')
+		# Save image with chosen colors and 
 		plt.imsave(sn,im_flux,
 			vmin=minv,vmax=max_bright_flux,
 			cmap=cmap)
